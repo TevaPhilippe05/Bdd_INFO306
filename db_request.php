b0631d960ed1363b71d4a7d50a1f5aae94c9f358
Ezacuak <antonin.plard@protonmail.com>
2024-12-03 14:45:23 +0100

    first sql request

--- db_request.php
+++ db_request.php
@@ -5,6 +5,8 @@ include_once "bdd.php";
 $bdd = db();
 
 function user_connect($mail, $psw) {
-    $query = "SELECT * FROM Etudiant WHERE {$mail}"
+    $query = "SELECT * FROM Etudiant WHERE {$mail}";
+    $out = mysqli_query($bdd, $query);
+    return $out;
 }
 ?>
